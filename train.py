2fd242580d3440d9f4839853eefb9898f7301598
Arunima Mittra <arunima1820@gmail.com>
2022-05-10 16:32:39 -0500

    first commit

---
+++ train.py
@@ -0,0 +1,19 @@
+import keras
+import numpy as np
+import pandas as pd
+import tensorflow as ts
+
+# TODO build custom hand data model
+# TODO create individual data file for each gesture sequence
+# TODO train a Reinforcement learning model (most likely best option)
+#       - categorizes the first frame and then uses the rest to reinforce the prediction
+#       - NOTE need to understand how to correct mistakes while going through the categories
+
+
+def main():
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
